230067ff3b9d14f287a1508b3099ed8f8d4d9137
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-01-13 13:15:19 -0600

    Added loops and functions ch 10

--- main.js
+++
@@ -1,7 +0,0 @@
-import { useFish } from './scripts/FishDataProvider.js'
-
-const allTheFish = useFish()
-
-for (const fish of allTheFish) {
-    console.log(fish)
-}
--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,1 +1,32 @@
-import 
+import { useFish } from './FishDataProvider.js'
+import { Fish } from './fish.js'
+/**
+ *  FishList which renders individual fish objects as HTML
+ */
+
+// TODO: Import `useFish` from the data provider module
+
+export const FishList = () => {
+
+    const contentElement = document.querySelector(".containerLeft__fishList")
+    const fishes = useFish()
+
+    // Generate all of the HTML for all of the fish
+    let fishHTMLRepresentations = ""
+    for (const fish of fishes) {
+        /*
+            Invoke the Fish component function
+            and pass the current fish object as an argument.
+            Each time, add the return value to the
+            fishHTMLRepresentations variable with `+=`
+        */
+       fishHTMLRepresentations += Fish(fish)
+    }
+
+    // Add a section, and all of the fish to the DOM
+    contentElement.innerHTML += `
+        <article class="fishList">
+            ${fishHTMLRepresentations}
+        </article>
+    `
+}
